1ae33cc55ce898ce741bf3653ba88eb7821b6852
Andrew Rothstein <andrewrothstein@Andrews-MacBook-Pro-261.local>
2026-03-27 22:09:49 -0400

    Final commit

--- project2_starter.py
+++ project2_starter.py
@@ -12,6 +12,8 @@
 # --- ARGUMENTS & EXPECTED RETURN VALUES PROVIDED --- #
 # --- SEE INSTRUCTIONS FOR FULL DETAILS ON METHOD IMPLEMENTATION --- #
 
+from webbrowser import Mozilla
+
 from bs4 import BeautifulSoup
 import re
 import os
@@ -256,7 +258,18 @@ def validate_policy_numbers(data) -> list[str]:
     # ==============================
     # YOUR CODE STARTS HERE
     # ==============================
-    pass
+    invalid=[]
+    pattern1= r"20\d{2}-00\d{4}STR"
+    pattern2=r"STR-\d{7}"
+    for row in data:
+        listing_id=row[1]
+        policy=row[2]
+        if policy in ["Pending", "Exempt"]:
+            continue
+        if not(re.fullmatch(pattern1, policy) or re.fullmatch(pattern2, policy)):
+            invalid.append(listing_id)
+    return invalid
+
     # ==============================
     # YOUR CODE ENDS HERE
     # ==============================
@@ -276,7 +289,20 @@ def google_scholar_searcher(query):
     # ==============================
     # YOUR CODE STARTS HERE
     # ==============================
-    pass
+    query= query.replace(" ", "+")
+    url= f"https://scholar.google.com/scholar?q={query}"
+    headers= {"User-Agent": "Mozilla/5.0"}
+    response= requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup= BeautifulSoup(response.text, 'html.parser')
+        titles=[]
+        for h3 in soup.find_all("h3", class_="gs_rt"):
+            title= h3.get_text()
+            titles.append(title)
+        return titles
+    else:
+        print("Failed to retrieve search results.")
+        return []
     # ==============================
     # YOUR CODE ENDS HERE
     # ==============================
@@ -293,7 +319,8 @@ class TestCases(unittest.TestCase):
     def test_load_listing_results(self):
         # TODO: Check that the number of listings extracted is 18.
         # TODO: Check that the FIRST (title, id) tuple is  ("Loft in Mission District", "1944564").
-        pass
+        self.assertEqual(len(self.listings), 18)
+        self.assertEqual(self.listings[0], ("Loft in Mission District", "1944564"))
 
     def test_get_listing_details(self):
         html_list = ["467507", "1550913", "1944564", "4614763", "6092596"]
@@ -304,14 +331,25 @@ class TestCases(unittest.TestCase):
         # 1) Check that listing 467507 has the correct policy number "STR-0005349".
         # 2) Check that listing 1944564 has the correct host type "Superhost" and room type "Entire Room".
         # 3) Check that listing 1944564 has the correct location rating 4.9.
-        pass
+        results= []
+        for listing in html_list:
+            results.append(get_listing_details(listing))
+        self.assertEqual(results[0]["467507"]["policy_number"], "STR-0005349")
+        self.assertEqual(results[2]["1944564"]["host_type"], "Superhost")
+        self.assertEqual(results[2]["1944564"]["room_type"], "Entire Room")
+        self.assertEqual(results[2]["1944564"]["location_rating"], 4.9)
+
+    
+
 
     def test_create_listing_database(self):
         # TODO: Check that each tuple in detailed_data has exactly 7 elements:
         # (listing_title, listing_id, policy_number, host_type, host_name, room_type, location_rating)
 
         # TODO: Spot-check the LAST tuple is ("Guest suite in Mission District", "467507", "STR-0005349", "Superhost", "Jennifer", "Entire Room", 4.8).
-        pass
+        for data in self.detailed_data:
+            self.assertEqual(len(data), 7)
+        self.assertEqual(self.detailed_date[-1], ("Guest suite in Mission District", "467507", "STR-0005349", "Superhost", "Jennifer", "Entire Room", 4.8))
 
     def test_output_csv(self):
         out_path = os.path.join(self.base_dir, "test.csv")
@@ -319,18 +357,26 @@ class TestCases(unittest.TestCase):
         # TODO: Call output_csv() to write the detailed_data to a CSV file.
         # TODO: Read the CSV back in and store rows in a list.
         # TODO: Check that the first data row matches ["Guesthouse in San Francisco", "49591060", "STR-0000253", "Superhost", "Ingrid", "Entire Room", "5.0"].
-
+        output_csv(self.detailed_data, out_path)
+        rows=[]
+        with open(out_path, 'r', encoding="utf-8-sig") as csvfile:
+            reader= csv.reader(csvfile)
+            for row in reader:
+                rows.append(row)
+        self.assertEqual(rows[1],["Guesthouse in San Francisco", "49591060", "STR-0000253", "Superhost", "Ingrid", "Entire Room", "5.0"])
         os.remove(out_path)
 
     def test_avg_location_rating_by_room_type(self):
         # TODO: Call avg_location_rating_by_room_type() and save the output.
         # TODO: Check that the average for "Private Room" is 4.9.
-        pass
+        averages= avg_location_rating_by_room_type(self.detailed_data)
+        self.assertEqual(averages["Private Room"], 4.9)
 
     def test_validate_policy_numbers(self):
         # TODO: Call validate_policy_numbers() on detailed_data and save the result into a variable invalid_listings.
         # TODO: Check that the list contains exactly "16204265" for this dataset.
-        pass
+        invalid_listings= validate_policy_numbers(self.detailed_data)
+        self.assertEqual(invalid_listings, ["16204265"])
 
 
 def main():
